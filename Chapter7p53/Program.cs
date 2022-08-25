13c87d792184e59f92b29c3fea79baecf2cba519
gidits13 <gidits13@gmail.com>
2022-08-25 17:42:30 +0300

    Добавьте файлы проекта.

---
+++ Chapter7p53/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Chapter7p53
+{
+    class Helper
+    {
+        public static void Swap(ref int a,ref int b)
+        {
+            int t;
+            t=a;
+            a = b;
+            b = t;   
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 3;
+            int num2 = 58;
+            Helper.Swap(ref num1, ref num2);
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            
+        }
+    }
+}
